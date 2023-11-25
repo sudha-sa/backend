92f413c821ecdd9196b63760022fd0ba58c80e55
Sudha Sahu <123008072+sudha-sa@users.noreply.github.com>
2023-11-25 19:11:38 +0530

    Update conn.js

--- medidekbackend/config/conn.js
+++ medidekbackend/config/conn.js
@@ -1,6 +1,6 @@
 const mongoose =require("mongoose");
 require("dotenv").config();
-// DgDkWKvh8pM3nPcw
+
 mongoose.connect("mongodb+srv://user:user@cluster0.k5f1upo.mongodb.net/medidekdatabase").then(()=>{
     console.log("connection succesfull")
 }).catch((e)=>{
